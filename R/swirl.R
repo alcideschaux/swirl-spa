a9ca62e8cc02575f3cc16046ad4cd731f55fe24a
Alcides Chaux <alcideschaux@uninorte.edu.py>
2015-03-26 06:43:16 -0400

    No skip

--- R/swirl.R
+++ R/swirl.R
@@ -1,46 +1,46 @@
 #' An interactive learning environment for R and statistics.
-#' 
+#'
 #' This function presents a choice of course lessons and interactively
 #' tutors a user through them. A user may be asked to watch a video, to
 #' answer a multiple-choice or fill-in-the-blanks question, or to
-#' enter a command in the R console precisely as if he or she were 
+#' enter a command in the R console precisely as if he or she were
 #' using R in practice. Emphasis is on the last, interacting with the
 #' R console. User responses are tested for correctness and hints are
 #' given if appropriate. Progress is automatically saved so that a user
 #' may quit at any time and later resume without losing work.
-#' 
+#'
 #' There are several ways to exit swirl: by typing \code{bye()} while in the R
 #' console, by hitting the Esc key while not in the R console, or by
-#' entering 0 from the swirl course menu. swirl will print a goodbye 
-#' message whenever it exits. 
-#' 
+#' entering 0 from the swirl course menu. swirl will print a goodbye
+#' message whenever it exits.
+#'
 #' While swirl is in operation, it may be controlled by entering special
-#' commands in the R console. One of the special commands is \code{bye()} 
+#' commands in the R console. One of the special commands is \code{bye()}
 #' as discussed above. Others are \code{play()}, \code{nxt()}, \code{skip()},
 #' and \code{info()}. The parentheses are important.
-#' 
+#'
 #' Sometimes a user will want to play around in the R console without
 #' interference or commentary from swirl. This can be accomplished by
 #' using the special command \code{play()}. swirl will remain in operation,
 #' silently, until the special command \code{nxt()} is entered.
-#' 
-#' The special command \code{skip()} can be used to skip a question if 
+#'
+#' The special command \code{skip()} can be used to skip a question if
 #' necessary. swirl will enter the correct answer and notify the
 #' user of the names of any new variables which it may have created
 #' in doing so. These may be needed for subsequent questions.
-#' 
+#'
 #' Finally, \code{info()} may be used to display a list of the special commands
 #' themselves with brief explanations of what they do.
 #' @param resume.class for development only; please accept the default.
 #' @param ... arguments for special purposes only, such as lesson testing
 #' @export
-#' @importFrom stringr str_c str_trim str_split str_length 
+#' @importFrom stringr str_c str_trim str_split str_length
 #' @importFrom stringr str_detect str_locate fixed str_split_fixed
-#' @importFrom testthat expectation equals is_equivalent_to 
+#' @importFrom testthat expectation equals is_equivalent_to
 #' @importFrom testthat is_identical_to is_a matches
 #' @examples
 #' \dontrun{
-#' 
+#'
 #' swirl()
 #' }
 swirl <- function(resume.class="default", ...){
@@ -78,7 +78,7 @@ swirl <- function(resume.class="default", ...){
 ## SPECIAL COMMANDS
 
 #' Exit swirl.
-#' 
+#'
 #' swirl operates by installing a callback function which responds
 #' to commands entered in the R console. This is how it captures
 #' and tests answers given by the user in the R console. swirl will
@@ -87,11 +87,11 @@ swirl <- function(resume.class="default", ...){
 #' @export
 #' @examples
 #' \dontrun{
-#' 
+#'
 #' | Create a new variable called `x` that contains the number 3.
-#' 
+#'
 #' > bye()
-#' 
+#'
 #' | Leaving swirl now. Type swirl() to resume.
 #' }
 bye <- function(){
@@ -101,52 +101,52 @@ bye <- function(){
 }
 
 #' Begin the upcoming question or unit of instruction.
-#' 
+#'
 #' This is the way to regain swirl's attention after viewing
-#' a video or \code{play()}'ing around in the console. 
+#' a video or \code{play()}'ing around in the console.
 #' @export
 #' @examples
 #' \dontrun{
-#' 
+#'
 #' | Create a new variable called `y` that contains the number 8.
-#' 
+#'
 #' > play()
-#' 
+#'
 #' | Entering play mode. Experiment as you please, then type nxt()
 #' | when you ready to resume the lesson.
-#' 
+#'
 #' > 10/14
 #' > [1] 0.7142857
 #' > zz <- 99
 #' > zz
 #' > [1] 99
 #' > nxt()
-#' 
+#'
 #' | Resuming lesson...
 #' }
 nxt <- function(){invisible()}
 
 #' Skip the current unit of instruction.
-#' 
+#'
 #' swirl will enter the correct answer and notify the
 #' user of the names of any new variables which it may have created
 #' in doing so. These may be needed for subsequent questions.
 #' @export
 #' @examples
 #' \dontrun{
-#' 
+#'
 #' | Create a new variable called `y` that contains the number 8.
-#' 
+#'
 #' > skip()
-#' 
+#'
 #' | I've entered the correct answer for you.
-#' 
+#'
 #' | In doing so, I've created the variable(s) y, which you may need later.
 #' }
 skip <- function(){invisible()}
 
 #' Start over on the current script question.
-#' 
+#'
 #' During a script question, this will reset the script
 #' back to its original state, which can be helpful if you
 #' get stuck.
@@ -154,78 +154,78 @@ skip <- function(){invisible()}
 reset <- function(){invisible()}
 
 #' Submit the active R script in response to a question.
-#' 
+#'
 #' When a swirl question requires the user to edit an R script, the
 #' \code{submit()} function allows the user to submit their response.
 #' @export
 #' @examples
 #' \dontrun{
-#' 
+#'
 #' | Create a function called f that takes one argument, x, and
 #' | returns the value of x squared.
-#' 
+#'
 #' > submit()
-#' 
+#'
 #' | You are quite good my friend!
 #' }
 submit <- function(){invisible()}
 
 #' Tell swirl to ignore console input for a while.
-#' 
+#'
 #' It is sometimes useful to play around in the R console out of
 #' curiosity or to solidify a concept. This command will cause
 #' swirl to remain idle, allowing the user to experiment at will,
-#' until the command \code{nxt()} is entered. 
+#' until the command \code{nxt()} is entered.
 #' @export
 #' @examples
 #' \dontrun{
-#' 
+#'
 #' | Create a new variable called `y` that contains the number 8.
-#' 
+#'
 #' > play()
-#' 
+#'
 #' | Entering play mode. Experiment as you please, then type nxt()
 #' | when you ready to resume the lesson.
-#' 
+#'
 #' > 10/14
 #' > [1] 0.7142857
 #' > zz <- 99
 #' > zz
 #' > [1] 99
 #' > nxt()
-#' 
+#'
 #' | Resuming lesson...
 #' }
 play <- function(){invisible()}
 
 #' Return to swirl's main menu.
-#' 
+#'
 #' Return to swirl's main menu from a lesson in progress.
 #' @export
 #' @examples
 #' \dontrun{
-#' 
+#'
 #' | The simplest way to create a sequence of numbers in R is by using
 #' | the `:` operator. Type 1:20 to see how it works.
-#' 
+#'
 #' > main()
-#' 
+#'
 #' | Returning to the main menu...
 #' }
 main <- function(){invisible()}
 
 #' Display a list of special commands.
-#' 
-#' Display a list of the special commands, \code{bye()}, \code{play()}, 
+#'
+#' Display a list of the special commands, \code{bye()}, \code{play()},
 #' \code{nxt()}, \code{skip()}, and \code{info()}.
 #' @export
 #' @examples
 #' \dontrun{
-#' 
+#'
 #' | Create a new variable called `z` that contains the number 11.
-#' 
+#'
 #' > info()
-#' 
+#'
 #' | When you are at the R prompt (>):
 #' | -- Typing skip() allows you to skip the current question.
 #' | -- Typing play() lets you experiment with R on your own; swirl will ignore what
@@ -233,14 +233,13 @@ main <- function(){invisible()}
 #' | -- UNTIL you type nxt() which will regain swirl's attention.
 #' | -- Typing bye() causes swirl to exit. Your progress will be saved.
 #' | -- Typing info() displays these options again.
-#' 
+#'
 #' > bye()
-#' 
+#'
 #' | Leaving swirl now. Type swirl() to resume.
 #' }
 info <- function(){
   swirl_out("Cuando estés en la línea de comandos de R (>):")
-  swirl_out("-- Tipear skip() te permite saltarte la pregunta actual.", skip_before=FALSE)
   swirl_out("-- Tipear play() te deja experimentar con R por tu cuenta; swirl ignorará lo que hagas ...", skip_before=FALSE)
   swirl_out("-- HASTA QUE tipees nxt() con lo que swirl prestará otra vez atención.", skip_before=FALSE)
   swirl_out("-- Tipear bye() hace que salgas de swirl. Se guardará tu progreso.", skip_before=FALSE)
@@ -253,12 +252,12 @@ info <- function(){
 
 resume <- function(...)UseMethod("resume")
 
-# Default method resume implements a finite state (or virtual) machine. 
-# It runs a fixed "program" consisting of three "instructions" which in 
-# turn present information, capture a user's response, and test and retry 
-# if necessary. The three instructions are themselves S3 methods which 
-# depend on the class of the active row of the course lesson. The 
-# instruction set is thus extensible. It can be found in R/instructionSet.R. 
+# Default method resume implements a finite state (or virtual) machine.
+# It runs a fixed "program" consisting of three "instructions" which in
+# turn present information, capture a user's response, and test and retry
+# if necessary. The three instructions are themselves S3 methods which
+# depend on the class of the active row of the course lesson. The
+# instruction set is thus extensible. It can be found in R/instructionSet.R.
 
 resume.default <- function(e, ...){
   # Check that if running in test mode, all necessary args are specified
@@ -290,7 +289,7 @@ resume.default <- function(e, ...){
       e$test_to <- targs$to
     }
   }
-  
+
   esc_flag <- TRUE
   on.exit(if(esc_flag)swirl_out("Saliendo de swirl ahora. Tipea swirl() para reiniciar.", skip_after=TRUE))
   # Trap special functions
@@ -298,7 +297,7 @@ resume.default <- function(e, ...){
     esc_flag <- FALSE
     return(TRUE)
   }
-  if(uses_func("nxt")(e$expr)[[1]]){    
+  if(uses_func("nxt")(e$expr)[[1]]){
     ## Using the stored list of "official" swirl variables and values,
     #  assign variables of the same names in the global environment
     #  their "official" values, in case the user has changed them
@@ -308,16 +307,16 @@ resume.default <- function(e, ...){
     e$playing <- FALSE
     e$iptr <- 1
   }
-  
+
   # The user wants to reset their script to the original
   if(uses_func("reset")(e$expr)[[1]]) {
     e$playing <- FALSE
     e$reset <- TRUE
     e$iptr <- 2
-    swirl_out("He reseteado el script a su estado original. Si no se actualizada inmediatamente, puede que necesites hacer click sobre el mismo.", 
+    swirl_out("He reseteado el script a su estado original. Si no se actualizada inmediatamente, puede que necesites hacer click sobre el mismo.",
               skip_after = TRUE)
   }
-  
+
   # The user wants to submit their R script
   if(uses_func("submit")(e$expr)[[1]]){
     e$playing <- FALSE
@@ -328,12 +327,12 @@ resume.default <- function(e, ...){
     swirl_out("Haciendo fuente tu script ...", skip_after = TRUE)
     try(source(e$script_temp_path))
   }
-  
+
   if(uses_func("play")(e$expr)[[1]]){
     swirl_out("Entrando al modo juego. Experimenta como se te plazca, luego tipea nxt() cuando estés listo para reiniciar la lección.", skip_after=TRUE)
     e$playing <- TRUE
   }
-  
+
   # If the user wants to skip the current question, do the bookkeeping.
   if(uses_func("skip")(e$expr)[[1]]){
     # Increment a skip count kept in e.
@@ -342,7 +341,7 @@ resume.default <- function(e, ...){
     # Enter the correct answer for the user
     # by simulating what the user should have done
     correctAns <- e$current.row[,"CorrectAnswer"]
-    
+
     # If we are on a script question, the correct answer should
     # simply source the correct script
     if(is(e$current.row, "script") && is.na(correctAns)) {
@@ -360,7 +359,7 @@ resume.default <- function(e, ...){
         file.edit(correct_script_path)
         readline("Presiona Enter cuando estés listo para continuar ...")
       }
-      
+
     # If this is not a script question...
     } else {
       # In case correctAns refers to newVar, add it
@@ -378,7 +377,7 @@ resume.default <- function(e, ...){
       e$val <- suppressMessages(suppressWarnings(evaluation$value))
       xfer(ce, globalenv())
       ce <- as.list(ce)
-      
+
       # Inform the user and expose the correct answer
       swirl_out("Ingresando la siguiente respuesta correcta por tí ...",
                 skip_after=TRUE)
@@ -388,17 +387,17 @@ resume.default <- function(e, ...){
         print(e$val)
       }
     }
-    
+
     # Make sure playing flag is off since user skipped
     e$playing <- FALSE
-    
-  # If the user is not trying to skip and is playing, 
+
+  # If the user is not trying to skip and is playing,
   # ignore console input, but remain in operation.
   } else if(exists("playing", envir=e, inherits=FALSE) && e$playing) {
     esc_flag <- FALSE
     return(TRUE)
-  }    
-  
+  }
+
   # If the user want to return to the main menu, do the bookkeeping
   if(uses_func("main")(e$expr)[[1]]){
     swirl_out("Regresando al menú principal ...")
@@ -407,10 +406,10 @@ resume.default <- function(e, ...){
       rm("les", envir=e, inherits=FALSE)
     }
   }
-  
+
   # If user is looking up a help file, ignore their input
   # unless the correct answer involves do so
-  if(uses_func("help")(e$expr)[[1]] || 
+  if(uses_func("help")(e$expr)[[1]] ||
        uses_func("`?`")(e$expr)[[1]]){
     # Get current correct answer
     corrans <- e$current.row[, "CorrectAnswer"]
@@ -424,7 +423,7 @@ resume.default <- function(e, ...){
       return(TRUE)
     }
   }
-  
+
   # Method menu initializes or reinitializes e if necessary.
   temp <- mainMenu(e)
   # If menu returns FALSE, the user wants to exit.
@@ -433,11 +432,11 @@ resume.default <- function(e, ...){
     esc_flag <- FALSE # To supress double notification
     return(FALSE)
   }
-  
+
   # if e$expr is NOT swirl() or nxt(), the user has just responded to
   # a question at the command line. Simulate evaluation of the
   # user's expression and save any variables changed or created
-  # in e$delta. 
+  # in e$delta.
   # TODO: Eventually make auto-detection of new variables an option.
   # Currently it can be set in customTests.R
   if(!uses_func("swirl")(e$expr)[[1]] &&
@@ -491,11 +490,11 @@ resume.default <- function(e, ...){
       }
     }
     # If we are ready for a new row, prepare it
-    if(e$iptr == 1){      
+    if(e$iptr == 1){
       # Increment progress bar
       cat("\n")
       setTxtProgressBar(e$pbar, e$pbar_seq[e$row])
-      
+
       #  Any variables changed or created during the previous
       #  question must have been correct or we would not be about
       #  to advance to a new row. Incorporate these in the list
@@ -507,10 +506,10 @@ resume.default <- function(e, ...){
       saveProgress(e)
       e$current.row <- e$les[e$row,]
       # Prepend the row's swirl class to its class attribute
-      class(e$current.row) <- c(e$current.row[,"Class"], 
+      class(e$current.row) <- c(e$current.row[,"Class"],
                                        class(e$current.row))
     }
-    
+
     # Execute the current instruction
     e$instr[[e$iptr]](e$current.row, e)
     # Check if a side effect, such as a sourced file, has changed the
@@ -523,7 +522,7 @@ resume.default <- function(e, ...){
       }
     }
   }
-  
+
   e$prompt <- FALSE
   esc_flag <- FALSE
   return(TRUE)
